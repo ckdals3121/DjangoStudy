cbbf50cdfd1a651170120efe10e1d6da419ef6b0
Desmos <ckdals3121@naver.com>
2024-04-17 10:34:24 +0200

    TEST post detail page

--- blog/tests.py
+++ blog/tests.py
@@ -47,3 +47,32 @@ class TestView(TestCase) :
         self.assertIn(post_002.title, main_area.text)
         # there's no "아직 게시물이 없습니다"
         self.assertNotIn("아직 게시물이 없습니다", main_area.text)
+
+    def test_post_detail(self) :
+        # There's one Post
+        post_001 = Post.objects.create(
+            title = "First Post",
+            content = "It's First",
+        )
+        # that Post's url is /blog/1
+        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+
+        # first post's detail page test
+        # if get first post's content with url, it work correctly
+        response = self.client.get(post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        # There's same nav bar 
+        navbar = soup.nav
+        self.assertIn("Blog", navbar.text)
+        self.assertIn("About Me", navbar.text)
+        # First post's title is in browser tab's title
+        self.assertIn(post_001.title, soup.title.text)
+        # First post's content is in post-area
+        main_area = soup.find('div', id = "main-area")
+        post_area = main_area.find('div', id = "post-area")
+        self.assertIn(post_001.title, post_area.text)
+        # First post's author is in post-area
+
+        # First post's content is in post-area
+        self.assertIn(post_001.content, post_area.text)
