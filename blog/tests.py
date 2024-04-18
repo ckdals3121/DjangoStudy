1c33ac61755945e7fc86097132cbdda79648af31
Desmos <ckdals3121@naver.com>
2024-04-18 09:13:14 +0200

    ADD author field in post list & post detail page

--- blog/tests.py
+++ blog/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
+from django.contrib.auth.models import User
 from .models import Post
 
 class TestView(TestCase) :
     def setUp(self) :
         self.client = Client()
+        self.user_desmos = User.objects.create_user(username = "desmos", password = 'pass')
+        self.user_mathway = User.objects.create_user(username = "mathway", password = "pass")
 
     def test_post_list(self) :
         # get post list 
@@ -27,10 +30,12 @@ class TestView(TestCase) :
         post_001 = Post.objects.create(
             title = "First Post",
             content = "Hello, It's First",
+            author = self.user_desmos
         )
         post_002 = Post.objects.create(
             title = "Second Post",
             content = "Hello, It's Second",
+            author = self.user_mathway
         )
         self.assertEqual(Post.objects.count(), 2)
 
@@ -45,11 +50,16 @@ class TestView(TestCase) :
         # there's no "아직 게시물이 없습니다"
         self.assertNotIn("아직 게시물이 없습니다", main_area.text)
 
+        # check author name
+        self.assertIn(self.user_desmos.username.upper(), main_area.text)
+        self.assertIn(self.user_mathway.username.upper(), main_area.text)
+
     def test_post_detail(self) :
         # There's one Post
         post_001 = Post.objects.create(
             title = "First Post",
             content = "It's First",
+            author = self.user_desmos
         )
         # that Post's url is /blog/1
         self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
@@ -68,7 +78,7 @@ class TestView(TestCase) :
         post_area = main_area.find('div', id = "post-area")
         self.assertIn(post_001.title, post_area.text)
         # First post's author is in post-area
-
+        self.assertIn(self.user_desmos.username.upper(), post_area.text)
         # First post's content is in post-area
         self.assertIn(post_001.content, post_area.text)
 
