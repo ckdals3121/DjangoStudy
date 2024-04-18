79e6638a83a6e782e50a9b144eb76c372fcb7687
Desmos <ckdals3121@naver.com>
2024-04-18 06:59:29 +0200

    Modularization navbar & footer

--- blog/tests.py
+++ blog/tests.py
@@ -14,11 +14,8 @@ class TestView(TestCase) :
         # page title is "Blog"
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, "Blog")
-        # have nav bar
-        navbar = soup.nav
-        # "Blog", "About Me" is in nav
-        self.assertIn("Blog", navbar.text)
-        self.assertIn("About Me", navbar.text)
+        # have nav bar, "Blog", "About Me" is in nav
+        self.navbar_test(soup)
 
         # if there's no one Post
         self.assertEqual(Post.objects.count(), 0)
@@ -63,9 +60,7 @@ class TestView(TestCase) :
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
         # There's same nav bar 
-        navbar = soup.nav
-        self.assertIn("Blog", navbar.text)
-        self.assertIn("About Me", navbar.text)
+        self.navbar_test(soup)
         # First post's title is in browser tab's title
         self.assertIn(post_001.title, soup.title.text)
         # First post's content is in post-area
@@ -76,3 +71,20 @@ class TestView(TestCase) :
 
         # First post's content is in post-area
         self.assertIn(post_001.content, post_area.text)
+
+    def navbar_test(self, soup) :
+        navbar = soup.nav
+        self.assertIn("Blog", navbar.text)
+        self.assertIn("About Me", navbar.text)
+
+        logo_btn = navbar.find('a', text = "Do It Django")
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn = navbar.find('a', text = "Home")
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text = 'Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text = 'About Me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
