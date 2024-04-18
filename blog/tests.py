b58e9cbe3b7de8113ae58b9062199c5cc98d2993
Desmos <ckdals3121@naver.com>
2024-04-18 10:28:51 +0200

    ADD category function in post list page

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post
+from .models import Post, Category
 
 class TestView(TestCase) :
     def setUp(self) :
@@ -9,50 +9,115 @@ class TestView(TestCase) :
         self.user_desmos = User.objects.create_user(username = "desmos", password = 'pass')
         self.user_mathway = User.objects.create_user(username = "mathway", password = "pass")
 
-    def test_post_list(self) :
-        # get post list 
-        response = self.client.get('/blog/')
-        # page load successfully
-        self.assertEqual(response.status_code, 200)
-        # page title is "Blog"
-        soup = BeautifulSoup(response.content, 'html.parser')
-        self.assertEqual(soup.title.text, "Blog")
-        # have nav bar, "Blog", "About Me" is in nav
-        self.navbar_test(soup)
+        self.category_programming = Category.objects.create(name = "programming", slug = "programming")
+        self.category_music = Category.objects.create(name = "music", slug = "music")
 
-        # if there's no one Post
-        self.assertEqual(Post.objects.count(), 0)
-        # "아직 게시물이 없습니다" is in main area
-        main_area = soup.find('div', id = "main-area")
-        self.assertIn('아직 게시물이 없습니다', main_area.text)
-
-        # it there are two Posts
-        post_001 = Post.objects.create(
+        self.post_001 = Post.objects.create(
             title = "First Post",
             content = "Hello, It's First",
+            category = self.category_programming,
             author = self.user_desmos
         )
-        post_002 = Post.objects.create(
+        self.post_002 = Post.objects.create(
             title = "Second Post",
             content = "Hello, It's Second",
+            category = self.category_music,
             author = self.user_mathway
         )
-        self.assertEqual(Post.objects.count(), 2)
+        self.post_003 = Post.objects.create(
+            title = "Third Post",
+            content = "Hello, It's Third",
+            author = self.user_desmos
+        )
 
-        # if refresh post list page
+    def category_card_test(self, soup) :
+        categories_card = soup.find('div', id = "categories-card")
+        self.assertIn("Categories", categories_card.text)
+        self.assertIn(f'{self.category_programming.name} ({self.category_programming.post_set.count()})', categories_card.text)
+        self.assertIn(f'{self.category_music.name} ({self.category_music.post_set.count()})', categories_card.text)
+        self.assertIn(f'미분류 (1)', categories_card.text)
+
+    def test_post_list(self) :
+        # # get post list 
+        # response = self.client.get('/blog/')
+        # # page load successfully
+        # self.assertEqual(response.status_code, 200)
+        # # page title is "Blog"
+        # soup = BeautifulSoup(response.content, 'html.parser')
+        # self.assertEqual(soup.title.text, "Blog")
+        # # have nav bar, "Blog", "About Me" is in nav
+        # self.navbar_test(soup)
+
+        # # if there's no one Post
+        # self.assertEqual(Post.objects.count(), 0)
+        # # "아직 게시물이 없습니다" is in main area
+        # main_area = soup.find('div', id = "main-area")
+        # self.assertIn('아직 게시물이 없습니다', main_area.text)
+
+        # # it there are two Posts
+        # post_001 = Post.objects.create(
+        #     title = "First Post",
+        #     content = "Hello, It's First",
+        #     author = self.user_desmos
+        # )
+        # post_002 = Post.objects.create(
+        #     title = "Second Post",
+        #     content = "Hello, It's Second",
+        #     author = self.user_mathway
+        # )
+        # self.assertEqual(Post.objects.count(), 2)
+
+        # # if refresh post list page
+        # response = self.client.get('/blog/')
+        # soup = BeautifulSoup(response.content, 'html.parser')
+        # self.assertEqual(response.status_code, 200)
+        # # there are two Post titles in main area
+        # main_area = soup.find('div', id = "main-area")
+        # self.assertIn(post_001.title, main_area.text)
+        # self.assertIn(post_002.title, main_area.text)
+        # # there's no "아직 게시물이 없습니다"
+        # self.assertNotIn("아직 게시물이 없습니다", main_area.text)
+
+        # # check author name
+        # self.assertIn(self.user_desmos.username.upper(), main_area.text)
+        # self.assertIn(self.user_mathway.username.upper(), main_area.text)
+
+        # 1 more Post
+        self.assertEqual(Post.objects.count(), 3)
+
         response = self.client.get('/blog/')
-        soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(response.status_code, 200)
-        # there are two Post titles in main area
-        main_area = soup.find('div', id = "main-area")
-        self.assertIn(post_001.title, main_area.text)
-        self.assertIn(post_002.title, main_area.text)
-        # there's no "아직 게시물이 없습니다"
-        self.assertNotIn("아직 게시물이 없습니다", main_area.text)
+        soup = BeautifulSoup(response.content, "html.parser")
 
-        # check author name
+        self.navbar_test(soup)
+        self.category_card_test(soup)
+
+        main_area = soup.find('div', id = "main-area")
+        self.assertNotIn('아직 게시물이 없습니다', main_area.text)
+
+        post_001_card = main_area.find('div', id = "post-1")
+        self.assertIn(self.post_001.title, post_001_card.text)
+        self.assertIn(self.post_001.category.name, post_001_card.text)
+
+        post_002_card = main_area.find('div', id = "post-2")
+        self.assertIn(self.post_002.title, post_002_card.text)
+        self.assertIn(self.post_002.category.name, post_002_card.text)
+
+        post_003_card = main_area.find('div', id = "post-3")
+        self.assertIn(self.post_003.title, post_003_card.text)
+        self.assertIn("미분류", post_003_card.text)
+
         self.assertIn(self.user_desmos.username.upper(), main_area.text)
         self.assertIn(self.user_mathway.username.upper(), main_area.text)
+
+        #  No Post
+        Post.objects.all().delete()
+        self.assertEqual(Post.objects.count(), 0)
+        response = self.client.get('/blog/')
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        main_area = soup.find('div', id = "main-area")
+        self.assertIn('아직 게시물이 없습니다', main_area.text)
 
     def test_post_detail(self) :
         # There's one Post
